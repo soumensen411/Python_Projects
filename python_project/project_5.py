60f493942c2a7e2abe7220473451bd2491c05e21
soumen <soumensen411@gmail.com>
2025-02-13 12:03:46 +0600

    Fifth Project

---
+++ python_project/project_5.py
@@ -0,0 +1,19 @@
+# shopping cart program 
+foods = []
+prices = []
+total = 0
+while True:
+    food = input("Enter food to buy (q to quit): ")
+    if food.lower() == 'q':
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}: $"))
+        foods.append(food)
+        prices.append(price)
+print("********** YOUR CART ********")
+for food in foods:
+    print(food,end=' ')
+print()
+for price in prices:
+    total = total + price
+print(f"Your total is ${total}")
